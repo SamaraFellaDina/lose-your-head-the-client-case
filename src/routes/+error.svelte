af83c5f1245cf039c754aa322470d0983c3ee17f
SamaraFellaDina <sammybensink@gmail.com>
2024-10-07 16:08:04 +0200

    Maken van een 'error.svelte' #35

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script>
+  import {page} from '$app/stores'
+
+  // console.log($page.status)
+</script>
+
+<section>
+  <h1>Kan de pagina niet vinden!</h1>
+  <p>{$page.status} - {$page.error.message}</p>
+  <a href="/">Naar home</a>
+</section>
+
+<style>
+  section {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1em;
+  }
+</style>
